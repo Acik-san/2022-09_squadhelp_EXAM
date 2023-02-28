80176ef033628b66bc612c73375b6f28c9801e8b
Acik <kodgiroartur228@gmail.com>
2023-02-28 11:59:16 +0200

    Add seeder of contests, made all table queries

--- DB_SQL/DB_SQL.sql
+++ DB_SQL/DB_SQL.sql
@@ -1,3 +1,4 @@
+/* DB-SQL task:1 */
 DROP TABLE IF EXISTS "conversations" 
 DROP TABLE IF EXISTS "users_to_conversations" 
 DROP TABLE IF EXISTS "messages" 
@@ -8,14 +9,14 @@ CREATE TABLE "conversations"(
   "id" SERIAL PRIMARY KEY,
   "createdAt" TIMESTAMP DEFAULT current_timestamp,
   "updatedAt" TIMESTAMP DEFAULT current_timestamp
-) 
+);
 CREATE TABLE "users_to_conversations"(
   "conversationId" INT REFERENCES "conversations"("id"),
   "userId" INT REFERENCES "users"("id"),
   "blackList" BOOLEAN NOT NULL,
   "favoriteList" BOOLEAN NOT NULL,
   PRIMARY KEY ("conversationId","userId")
-)
+);
 CREATE TABLE "messages"(
   "id" SERIAL PRIMARY KEY,
   "body" VARCHAR(512) NOT NULL CHECK("body"!=''),
@@ -24,16 +25,43 @@ CREATE TABLE "messages"(
   FOREIGN KEY ("conversationId","sender") REFERENCES "users_to_conversations"("conversationId","userId"),
   "createdAt" TIMESTAMP DEFAULT current_timestamp,
   "updatedAt" TIMESTAMP DEFAULT current_timestamp
-)
+);
 CREATE TABLE "catalogs"(
   "id" SERIAL PRIMARY KEY,
   "userId" INT REFERENCES "users"("id"),
   "catalogName" VARCHAR(32) NOT NULL CHECK("catalogName"!='')
-)
+);
 CREATE TABLE "catalogs_to_conversations"(
   "conversationId" INT REFERENCES "conversations"("id"),
   "catalogId" INT REFERENCES "catalogs"("id"),
   PRIMARY KEY ("conversationId","catalogId")
-)
+);
 
+/* DB-SQL task:2 */
+SELECT "role", count(*)
+FROM "users"
+WHERE "role" IN ('creator','customer')
+GROUP BY "role"
+
+/* DB-SQL task:3 */
+UPDATE "users"
+SET "balance" = "balance" + ("contests"."total"*0.1)
+FROM (
+  SELECT "userId",SUM("prize") AS "total"
+  FROM "contests"
+  WHERE "createdAt"::date BETWEEN '2022-12-25'::date AND '2023-01-14'::date
+  GROUP BY "userId" 
+) AS "contests"
+WHERE "users"."id"="contests"."userId"
+
+/* DB-SQL task:4 */
+UPDATE "users"
+SET "balance" = "balance" + 10
+WHERE "id" IN (
+  SELECT "id"
+  FROM "users"
+  WHERE "role" = 'creator' 
+  ORDER BY "rating" DESC
+  LIMIT 3
+)
 
---
+++ server/src/seeders/20220216144850-insert-contests.js
@@ -0,0 +1,31 @@
+const _ = require('lodash');
+const { User } = require('../models');
+const { v4: uuid } = require('uuid')
+
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const users = await User.findAll({ where: { role: "customer" }, attributes: ['id'] })
+    const generateContest = key => ({
+      id: key,
+      contestType: _.sample(['name', 'tagline', 'logo']),
+      status: "finished",
+      prize: 100,
+      createdAt: _.sample(["2022-12-25 12:00:00+00", "2022-12-27 12:00:00+00", "2022-12-30 12:00:00+00", "2022-12-31 12:00:00+00", "2023-01-03 12:00:00+00", "2023-01-09 12:00:00+00", "2023-01-14 12:00:00+00", "2023-01-24 12:00:00+00", "2023-02-02 12:00:00+00", "2023-02-05 12:00:00+00",]),
+      priority: 1,
+      orderId: uuid(),
+      userId: _.sample(users.map((user) => user.dataValues.id)),
+    });
+
+    const generateContests = (amount) => {
+      return new Array(amount > 50 ? 50 : amount)
+        .fill(null)
+        .map((e, i) => generateContest(i));
+    };
+    await queryInterface.bulkInsert('contests', generateContests(50), {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('contests', null, {});
+  },
+};
